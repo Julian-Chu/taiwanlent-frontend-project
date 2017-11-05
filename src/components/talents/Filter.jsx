f0d1694edc96c90a891013202027233afe11d4ee
JulianChu <yulang.chu@gmail.com>
2017-11-05 21:48:42 +0100

    add regions select

--- src/components/talents/Filter.jsx
+++ src/components/talents/Filter.jsx
@@ -2,90 +2,125 @@ import React, { Component } from 'react';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 
+
+
 export default class Filter extends Component {
   constructor(props) {
     super(props);
 
-    this.langOptions = [      
+    this.langOptions = [
       { value: 'de', label: '德文' },
       { value: 'en', label: '英文' },
       { value: 'ch', label: '中文' }
     ];
 
-    this.state={
-        selectedLanguages:'',
-        selectAllLangsIsChecked: false,
+    this.regionOptions = [
+      { value: 'BW', label: 'Baden-Württemberg 巴登-符騰堡' },
+      { value: 'BE', label: 'Berlin 柏林' },
+      { value: 'BY', label: 'Bavaria 巴伐利亞' },
+      { value: 'HB', label: 'Bremen 布萊梅' },
+      { value: 'BB', label: 'Brandenburg 布蘭登堡' },
+      { value: 'HH', label: 'Hamburg 漢堡' },
+      { value: 'HE', label: 'Hessen 黑森' },
+      { value: 'NI', label: 'Lower Saxony 下薩克森' },
+      { value: 'SL', label: 'Saarland 薩爾蘭' },
+      { value: 'SN', label: 'Saxony 薩克森' },
+      { value: 'ST', label: 'Saxony-Anhalt 薩克森-安哈特' },
+      { value: 'TH', label: 'Thuringia 圖林根' },
+      { value: 'RP', label: 'Rhineland-Palatinate 萊茵蘭-普法茲' },
+      { value: 'SH', label: 'Schleswig-Holstein 史列斯威-霍爾斯' },
+      { value: 'NW', label: 'North Rhine-Westphalia 北萊茵-西法冷' },
+      { value: 'MV', label: 'Mecklenburg-Western Pomerania 梅克倫堡-西波美恩' },
+    ]
+
+
+    this.state = {
+      selectedLanguages: [],
+      selectAllLangsIsChecked: false,
+      selectedRegions: [],
+      selectAllRegionsIsChecked: false,
     }
   }
 
-  logChange(val) {
-    console.log('Selected: ', val);
+  langsChange(val) {
     this.setState({
       selectedLanguages: val,
     })
   }
 
-  selectAllLangs(){
-    console.log(this.state.selectAllLangsIsChecked);
-
-    if(this.state.selectAllLangsIsChecked){
+  selectAllLangs() {
+    if (this.state.selectAllLangsIsChecked) {
       this.setState({
         selectedLanguages: [],
         selectAllLangsIsChecked: false
       })
-    }else{
+    } else {
       this.setState({
         selectedLanguages: this.langOptions,
         selectAllLangsIsChecked: true
       })
     }
-   
+  }
+
+  regionsChange(val) {
+    this.setState({
+      selectedRegions: val,
+    })
+  }
+
+  selectAllRegions() {
+    if (this.state.selectAllRegionsIsChecked) {
+      this.setState({
+        selectedRegions: [],
+        selectAllRegionsIsChecked: false
+      })
+    } else {
+      this.setState({
+        selectedRegions: this.regionOptions,
+        selectAllRegionsIsChecked: true
+      })
+    }
   }
 
   render() {
     return (
       <div>
-        <Select          
+      <div style={{ display: 'inline-block', width: '400px', maxHeight:'20px' }}>
+        <Select
+          multi
+          options={this.regionOptions}
+          onChange={(val) => this.regionsChange(val)}
+          placeholder="Select regions"
+          closeOnSelect={false}
+          value={this.state.selectedRegions}
+          style={{ width: '400px', maxHeight:'20px', overflow:'hidden' }}
+        />
+        <label htmlFor="">
+          <input type="checkbox" name="selectAllRegions" checked={this.state.selectAllRegionsIsChecked} onChange={() => this.selectAllRegions()} />
+          <span className="checkbox-label">全選</span>
+        </label>
+        </div>
+
+        <div style={{ display: 'inline-block', maxWidth: '400px' }}>
+        <Select
           multi
           options={this.langOptions}
           style={{ maxWidth: '250px' }}
-          onChange={ (val)=>this.logChange(val)}
+          onChange={(val) => this.langsChange(val)}
           placeholder="Select items"
           closeOnSelect={false}
-          rtl={false}
           value={this.state.selectedLanguages}
         />
         <label htmlFor="" className="checkbox">
-          <input type="checkbox" name="selectAllLangs" checked={this.state.selectAllLangsIsChecked} onChange={(val)=>this.selectAllLangs(val)}/>
+          <input type="checkbox" name="selectAllLangs" checked={this.state.selectAllLangsIsChecked} onChange={() => this.selectAllLangs()} />
           <span className="checkbox-label">全選</span>
         </label>
-        <ul id="portfolio-filter" className="portfolio-filter clearfix">
-          <li style={{ minWidth: '50px' }}>
+        </div>
 
-          </li>
 
-          <li><span>&nbsp</span></li>
-          {/* <li>
-          <select name="multicheckbox[]" id="selectRegion" multiple="multiple" style={{ display: 'inline-block', width: '90%' }}>
-            <option value="all">all</option>
-            <option value="BW">Baden-Württemberg<br /> 巴登-符騰堡</option>
-            <option value="BE">Berlin柏林</option>
-            <option value="BY">Bavaria (lozengy)巴伐利亞 </option>
-            <option value="HB">Bremen布萊梅</option>
-            <option value="BB">Brandenburg布蘭登堡 </option>
-            <option value="HH">Hamburg 漢堡</option>
-            <option value="HE">Hessen 黑森 </option>
-            <option value="NI">Lower Saxony 下薩克森 </option>
-            <option value="SL">Saarland 薩爾蘭</option>
-            <option value="SN">Saxony 薩克森</option>
-            <option value="ST">Saxony-Anhalt 薩克森-安哈特</option>
-            <option value="TH">Thuringia 圖林根</option>
-            <option value="RP">Rhineland-Palatinate 萊茵蘭-普法茲</option>
-            <option value="SH">Schleswig-Holstein 史列斯威-霍爾斯坦</option>
-            <option value="NW">North Rhine-Westphalia 北萊茵-西法冷</option>
-            <option value="MV">Mecklenburg-Western Pomerania 梅克倫堡-西波美恩</option>
-          </select>
-        </li> */}
+
+        {/* <ul id="portfolio-filter" className="portfolio-filter clearfix">
+          
           <li><span>&nbsp</span></li>
 
           <li>
@@ -102,7 +137,7 @@ export default class Filter extends Component {
               </select>
             </div>
           </li>
-        </ul>
+        </ul> */}
       </div>
     )
   }

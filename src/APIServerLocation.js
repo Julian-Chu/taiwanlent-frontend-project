1887cd652e503ee35dcf0e377151876e12a3540e
JulianChu <yulang.chu@gmail.com>
2018-01-28 12:45:20 +0100

    add json server in Azure

--- src/APIServerLocation.js
+++ src/APIServerLocation.js
@@ -1,3 +1,4 @@
-const APIServerLocation = "http://localhost:3000";
+// const APIServerLocation = "http://localhost:3000";
+const APIServerLocation = "https://testjsonserver.azurewebsites.net/"
 // APIServerLocation = ""; //the same domain
 export default APIServerLocation;

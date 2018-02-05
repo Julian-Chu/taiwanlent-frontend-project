16db0aea3bec8355b2aa4f8619b1a6c739893614
JulianChu <yulang.chu@gmail.com>
2018-02-05 23:15:08 +0100

    remove redundant code

--- src/actions/post_ContactUs.js
+++ src/actions/post_ContactUs.js
@@ -3,7 +3,7 @@ import APIServerLocation from '../APIServerLocation';
 
 export const Post_ContactUs = 'Post_ContactUs';
 
-export default function post_ContactUs(){
+export default function post_ContactUs(values){
   console.log("post_ContactUs in Action Creator")
   const request = axios.post(`${APIServerLocation}/contactus`, values)
                   .then(()=>callback());
--- src/components/home/SectionContact.jsx
+++ src/components/home/SectionContact.jsx
@@ -6,148 +6,13 @@ import checkRules from '../../regularExpression/checkRules';
 import postContactUs from '../../actions/post_EmailSubscribe';
 
 class SectionContact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            email: "",
-            phone: "",
-            subject: "",
-            message: ""
-        };
-
-        this.state = {
-            email: "",
-            phone: "",
-            emailStyle: {
-                borderColor: '',
-                warning: {
-                    display: 'none'
-                }
-            },
-            phoneStyle: {
-                borderColor: '',
-                warning: {
-                    display: 'none'
-                }
-            },
-        };
-
-        this.validateTelephone = this.validateTelephone.bind(this);
-        this.validateEmail = this.validateEmail.bind(this);
-
-        this.onUsernameChangeEvent = this.onUsernameChangeEvent.bind(this);
-        this.onEmailChangeEvent = this.onEmailChangeEvent.bind(this);
-        this.onPhoneChangeEvent = this.onPhoneChangeEvent.bind(this);
-
-        this.doValidateEmail = _.debounce(this.doValidateEmail, 300);
-        this.doValidatePhone = _.debounce(this.doValidatePhone, 300);
-    }
-
-    validateEmail(email) {
-        var re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
-
-    validateTelephone(phone) {
-        var re = /^\+\d{1,3}-\d{8,13}/;
-        return re.test(phone)
-    }
-
-    onUsernameChangeEvent(e) {
-        let name = e.target.value;
-        this.setState({ username: name });
-    }
-
-    onEmailChangeEvent(e) {
-        let email = e.target.value;
-        this.setState({ email });
-        this.doValidateEmail(email);
-    }
-
-    doValidateEmail(email) {
-        if (email !== '') {
-            this.setState({
-                emailStyle: this.validateEmail(email) ?
-                    {
-                        borderColor: '',
-                        warning: {
-                            display: 'none'
-                        }
-                    } :
-                    {
-                        borderColor: 'red',
-                        warning: {
-                            display: 'none'
-                        }
-                    },
-            })
-        }
-        else {
-            this.setState({
-                emailStyle: {
-                    borderColor: '',
-                    warning: {
-                        display: 'none'
-                    }
-                }
-            });
-        }
-    }
-
-    onPhoneChangeEvent(e) {
-        let phone = e.target.value;
-        this.setState({ phone: phone });
-        this.doValidatePhone(phone);
-
-    }
-
-    doValidatePhone(phone) {
-        if (phone !== '')
-            this.setState({
-                phoneStyle: this.validateTelephone(phone) ?
-                    {
-                        borderColor: '',
-                        warning: {
-                            display: 'none'
-                        }
-                    } :
-                    {
-                        borderColor: 'red',
-                        warning: {
-                            display: 'none'
-                        }
-                    },
-            })
-        else this.setState({
-            phoneStyle: {
-                borderColor: '',
-                warning: {
-                    display: 'none'
-                }
-            }
-        })
-
-    }
-
-    onSubjectChangeEvent(e) {
-        let subject = e.target.value;
-        this.setState({ subject });
-    }
-
-    onMessageChangeEvent(e) {
-        let message = e.target.value;
-        this.setState({ message });
-    }
-
     onSubmit(values){
         console.log(values);
+        this.props.postContactUs(values);
     }
 
     renderField(field) {
         const { meta: { touched, error }, className } = field;
-        console.log(field);
-
         const divClassName = `form-group ${touched && error ? 'alert-danger' : ''}`;
         const styles = touched && error ? { borderColor: 'red' } : {};
         const inputClassName = `sm-form-control border-form-control required ${touched && error ? 'alert-danger' : ''} `;
@@ -213,36 +78,11 @@ class SectionContact extends Component {
                                     cols="30"                                    
                                     />
                                 <div className="clear"></div>
-                                
-                                <div className="col_half">
-                                    <input type="text" id="template-contactform-name" name="template-contactform-name" className="sm-form-control border-form-control required" placeholder="Name" value={this.state.username} onChange={this.onUsernameChangeEvent} maxLength="14" />
-                                </div>
-                                <div className="col_half col_last">
-                                    <input type="email" id="template-contactform-email" style={this.state.emailStyle} name="template-contactform-email" value={this.state.email} onChange={this.onEmailChangeEvent} className="required email sm-form-control border-form-control" placeholder="Email Address" />
-                                </div>
-
-                                <div className="clear"></div>
-
-                                <div className="col_one_third" >
-                                    <input type="text" id="template-contactform-phone" style={this.state.phoneStyle} name="template-contactform-phone" maxLength="16" value={this.state.phone} onChange={this.onPhoneChangeEvent} className="sm-form-control border-form-control" placeholder="Phone" />
-                                </div>
-
-                                <div className="col_two_third col_last">
-                                    <input type="text" id="template-contactform-subject" name="template-contactform-subject" value={this.state.subject} className="required sm-form-control border-form-control" placeholder="Subject" />
-                                </div>
-
-                                <div className="clear"></div>
-
-                                <div className="col_full">
-                                    <textarea className="required sm-form-control border-form-control" id="template-contactform-message" name="template-contactform-message" rows="7" cols="30" placeholder="Your Message" value={this.state.message}></textarea>
-                                </div>
-
                                 <div className="col_full center">
-                                    <button className="button button-border button-circle t500 noleftmargin topmargin-sm" type="submit" id="template-contactform-submit" name="template-contactform-submit" value="submit" disabled={submitting}>Send Message</button>
+                                    <button className="button button-border button-circle t500 noleftmargin topmargin-sm" type="submit"  value="submit" disabled={submitting}>Send Message</button>
                                     <br />
                                     <small style={{ display: 'block', fontSize: '13px', marginTop: '15px' }}>We'll do our best to get back to you as soon as possbile.</small>
                                 </div>
-
                                 <div className="clear"></div>
 
                                 <div className="col_full hidden">
@@ -265,7 +105,8 @@ function validate(values) {
     if(!values.subject) errors.subject = "Subject is empty";
     if(!values.message) errors.message ="Message is empty";
 
-    if(!checkRules.Email(values.email)) errors.email ="Invalid Email"
+    if(!checkRules.Email(values.email)) errors.email ="Invalid Email";
+    if(!checkRules.Phone(values.phone)) errors.phone = "Format: +XX-XXXXXXXXXX";
 
     return errors;
 }

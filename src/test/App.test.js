79692905f83c6a7622f2d2f5e9413e71fd0e2d04
JulianChu <yulang.chu@gmail.com>
2018-02-08 23:48:57 +0100

    add App.test.js and passed

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,20 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../reducers';
+
 
 
 describe('App', ()=>{
+
+  const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
-it('2+2=4',()=>{
-expect(2+2).toBe(4);
+  ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}><App/></Provider>, div);
 });
 
-it('check',()=>{
-  const App = shallow(<App/>).dive();
-  console.log(App);
+const app = shallow(<App/>);
+it('SidePanel exists',()=>{
+  expect(app.find('SidePanel').exists()).toBe(true);
+})
+
+it('Header exists',()=>{
+  expect(app.find('Header').exists()).toBe(true);
+})
+
+it('Footer exists',()=>{
+  expect(app.find('Footer').exists()).toBe(true);
 })
 
 });

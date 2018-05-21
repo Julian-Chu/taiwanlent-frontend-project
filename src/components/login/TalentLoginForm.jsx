baf94644b8c9e229aa97699b6a354c940aed91b7
JulianChu <yulang.chu@gmail.com>
2018-05-21 15:22:55 +0200

    reorganzie TalentLoginForm

--- src/components/login/TalentLoginForm.jsx
+++ src/components/login/TalentLoginForm.jsx
@@ -5,17 +5,12 @@ const TalentLoginForm = props => {
   return (
     <div>
       <h3>人才登入</h3>
-      <div className="col_full"></div>
-      <div className="col_full">
       <button className="loginBtn loginBtn--facebook">
-          Login with Facebook
+        Login with Facebook
       </button>
-      </div>
-      <div className="col_full">
-        <button className="loginBtn loginBtn--google">
-          Login with Google
+      <button className="loginBtn loginBtn--google">
+        Login with Google
       </button>
-      </div>
     </div>
   )
 }

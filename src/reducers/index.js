8087821f64013f81ea23286f2ae833b75b524040
JulianChu <yulang.chu@gmail.com>
2018-05-27 18:48:08 +0200

    add businessUserDataReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import FilterReducer from './reducer_filters';
 import CandidatesReducer from './reducer_candidates';
 import { reducer as formReducer} from 'redux-form';
 import AuthenticationReducer from './reducer_authentication';
+import BusinessUserDataReducer from './reducer_businessUserData';
 
 const rootReducer = combineReducers({
   talents: TalentsReducer,
   filter: FilterReducer,
   candidates:  CandidatesReducer,
   authenticated: AuthenticationReducer,
-  form:formReducer
+  form:formReducer,
+  businessUserData: BusinessUserDataReducer
 });
 
 export default rootReducer;

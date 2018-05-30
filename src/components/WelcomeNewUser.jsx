9b717b7a2c27ab74d3e47922be62fba5a325d800
JulianChu <yulang.chu@gmail.com>
2018-05-30 23:34:45 +0200

    add WelcomeNewUser css

--- src/components/WelcomeNewUser.jsx
+++ src/components/WelcomeNewUser.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import require_auth from './require_authentication';
 import { Link } from 'react-router-dom';
+import '../styles/WelcomeNewUser.css';
 
 const WelcomeNewUser = () =>{
   return(
-    <div>
-      <h6>歡迎使用台灣瀾!</h6>
+    <div className="WelcomeNewUser">
+      <h2>歡迎使用台灣瀾!</h2>
       <Link to="/talents">前往人才資料庫</Link>      
     </div>
   )

836332129bb27a936730afdc849dac2583fc5a0e
JulianChu <yulang.chu@gmail.com>
2018-05-30 22:42:26 +0200

    add WelcomeNewUser component

---
+++ src/components/WelcomeNewUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import require_auth from './require_authentication';
+import { Link } from 'react-router';
+
+const WelcomeNewUser = () =>{
+  return(
+    <div>
+      <h6>歡迎使用台灣瀾!</h6>
+      <Link to="/talents">前往人才資料庫</Link>      
+    </div>
+  )
+};
+
+

0b2f96ef296cf971f6522655f27ff48341bdcd3b
JulianChu <yulang.chu@gmail.com>
2018-05-31 22:45:59 +0200

    add PersonalUserRegister

--- src/components/personalUser/PersonalUserRegister.jsx
+++ src/components/personalUser/PersonalUserRegister.jsx
@@ -3,10 +3,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import FirstPage from './PersonalUserRegister/FirstPage';
 import SecondPage from './PersonalUserRegister/SecondPage';
+import { reduxForm } from 'redux-form';
+import PropTypes from 'prop-types';
 
-class PersonalUserRegister extends Component{
-  constructor(props){
+class PersonalUserRegister extends Component {
+  constructor(props) {
     super(props);
-    
+    this.nextPage = this.nextPage.bind(this);
+    this.previousPage = this.previousPage.bind(this);
+    this.renderPageBuNumber = this.renderPageBuNumber.bind(this);
+    this.state = {
+      page:1
+    };
+
+  }
+  nextPage() {
+    this.setState({ page: this.state.page + 1 });
+  }
+
+  previousPage() {
+    this.setState({page:this.state.page - 1});
+  }
+
+  renderPageBuNumber(page, onSubmit){
+    switch(page){
+      case 1:
+        return(
+          <FirstPage onSubmit={this.nextPage}></FirstPage>
+        )
+      case 2:
+        return(
+          <SecondPage onSubmit={onSubmit}></SecondPage>
+        )
+    }
   }
 }
+
+
+export default reduxForm({
+  form: 'PersonalUserRegisterForm'
+})(PersonalUserRegister);

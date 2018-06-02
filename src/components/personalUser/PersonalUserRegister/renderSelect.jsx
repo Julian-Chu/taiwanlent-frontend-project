4f471b8337f6a195af8f7e62f87a9dd0d9d74548
JulianChu <yulang.chu@gmail.com>
2018-06-02 21:49:31 +0200

    add warning to renderSelect

--- src/components/personalUser/PersonalUserRegister/renderSelect.jsx
+++ src/components/personalUser/PersonalUserRegister/renderSelect.jsx
@@ -3,7 +3,8 @@ import Select from 'react-select';
 import React from 'react';
 
 const renderSelect = (field) => {
-  const { className, title } = field;
+    const { meta: { touched, error }, className, title } = field;
+    const divClassName = `form-group ${touched && error ? 'alert-danger' : ''}`;
   return (
     <div className={className} >
       <label htmlFor={field.name} style={{ display: 'block' }}>{title}</label>
@@ -17,6 +18,9 @@ const renderSelect = (field) => {
         onBlur={() => field.input.onBlur(field.input.value)}
         {...field.meta}
       />
+        <div className={`text-help ${divClassName}`}>
+          {touched ? error : ''}
+        </div>
     </div>
   )
 }

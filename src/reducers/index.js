b54cb55fcf4442ce90bf0978b076d4260bf14249
JulianChu <yulang.chu@gmail.com>
2018-06-05 23:30:15 +0200

    add personalUserData reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,7 @@ import CandidatesReducer from './reducer_candidates';
 import { reducer as formReducer} from 'redux-form';
 import AuthenticationReducer from './reducer_authentication';
 import BusinessUserDataReducer from './reducer_businessUserData';
+import PersonalUserDataReducer from './reducer_personalUserDate';
 
 const rootReducer = combineReducers({
   talents: TalentsReducer,
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
   candidates:  CandidatesReducer,
   authenticated: AuthenticationReducer,
   form:formReducer,
-  businessUserData: BusinessUserDataReducer
+  businessUserData: BusinessUserDataReducer,
+  peronsalUserData: PersonalUserDataReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_personalUserDate.js
@@ -0,0 +1,10 @@
+import { PERSONAL_USER_DATA} from '../actions/types';
+
+export default function(state={}, action){
+  switch(action.type){
+    case PERSONAL_USER_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+}

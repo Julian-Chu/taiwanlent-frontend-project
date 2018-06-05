548e0ae85324b7f488f36571d18ac2a25d267005
JulianChu <yulang.chu@gmail.com>
2018-06-05 23:34:05 +0200

    add GetPersonalUserData action creator

--- src/actions/personaluser.js
+++ src/actions/personaluser.js
@@ -1,4 +1,4 @@
-import {CHANGE_AUTH, PERSONAL_USER} from './types';
+import {CHANGE_AUTH, PERSONAL_USER, PERSONAL_USER_DATA} from './types';
 
 export function fillUpUserData(values, history){
   console.log(values);
@@ -6,5 +6,20 @@ export function fillUpUserData(values, history){
   return{
     type:CHANGE_AUTH,
     payload: PERSONAL_USER
+  }
+}
+
+export function GetPersonalUserData(){
+  var userdata = {
+    username: 'testUser',
+    gender:'male',
+    email:'test@test.com',
+    name:'Tom Lee',
+    phone:'1211313131',
+  }
+  console.log('GetPersonalBusinessUserData');
+  return{
+    type: PERSONAL_USER_DATA,
+    payload: userdata
   }
 }

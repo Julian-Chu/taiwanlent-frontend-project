df9d04b1184770b61d04afa48f95b5295a7e2ef2
JulianChu <yulang.chu@gmail.com>
2018-06-10 23:40:33 +0200

    first test with puppeteer

---
+++ src/test/integrationTest.test.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer')
+
+it('launch a browser', async()=>{
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('h2.nobottommargin.t300', el=>el.innerHTML);
+  expect(text).toEqual('Taiwanlent 提供您各領域的專業人才');
+})

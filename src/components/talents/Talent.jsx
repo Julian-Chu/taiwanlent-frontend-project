18f621bf331427f1250dd1827f2bebaeb31eeece
JulianChu <yulang.chu@gmail.com>
2018-08-29 22:16:15 +0200

    fix data binding in talents

--- src/components/talents/Talent.jsx
+++ src/components/talents/Talent.jsx
@@ -1,7 +1,9 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import '../../styles/talents/talent.css';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../../styles/talents/talent.css";
+import regions from "../common/regions";
+import subjects from "../common/subjects";
 
 class Talent extends Component {
   addToList() {
@@ -17,14 +19,39 @@ class Talent extends Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <div className="product clearfix pf-dress">
         <div className="product-image ">
-          <a href="# "><img src={this.props.photo?this.props.photo:(this.props.gender === "male"?'images/male.png':'images/female.png')} alt={this.props.name} /></a>
-          <div className={this.props.qualified ? 'sale-flash' : ''}> {this.props.qualified ? 'Qualified' : ''} </div>
-          <div className={this.props.experienced ? 'sale-flash' : ''}> {this.props.experienced ? 'Experienced' : ''}</div>
+          <a href="# ">
+            <img
+              src={
+                this.props.photo
+                  ? this.props.photo
+                  : this.props.gender === "male"
+                    ? "images/male.png"
+                    : "images/female.png"
+              }
+              alt={this.props.name}
+            />
+          </a>
+          <div className={this.props.qualified ? "sale-flash" : ""}>
+            {" "}
+            {this.props.qualified ? "Qualified" : ""}{" "}
+          </div>
+          <div className={this.props.experienced ? "sale-flash" : ""}>
+            {" "}
+            {this.props.experienced ? "Experienced" : ""}
+          </div>
           <div className="product-overlay ">
-            <button className="item-quick-view" onClick={this.props.authenticated?()=>this.props.enableDetail(this.props.id): alert('Please log in')}>
+            <button
+              className="item-quick-view"
+              onClick={
+                this.props.authenticated
+                  ? () => this.props.enableDetail(this.props.id)
+                  : alert("Please log in")
+              }
+            >
               <i className="icon-zoom-in2 " />
               <span> 查看資料</span>
             </button>
@@ -32,24 +59,68 @@ class Talent extends Component {
         </div>
         <div className="product-desc ">
           <div className="product-title ">
-            <h3><a href="# ">{this.props.name}</a></h3>
+            <h3>
+              <a href="# ">{this.props.name}</a>
+            </h3>
           </div>
           <div className="product-price ">
-            <div>語言:<span>{this.props.langs}</span></div>
-            <div>專業背景:<span>{this.props.subjectCategory}</span></div>
-            <div>所在地:<span>{this.props.region}</span></div>
+            <div>
+              語言:&nbsp;
+              <span>
+                {this.props.english ? "英文" : ""}
+                {this.props.english && (this.props.german || this.props.chinese)
+                  ? "/"
+                  : ""}
+                {this.props.german ? "德文" : ""}
+                {this.props.german && this.props.chinese ? "/" : ""}
+                {this.props.chinese ? "中文" : ""}
+              </span>
+            </div>
+            <div>
+              專業背景:&nbsp;
+              <span>
+                {
+                  subjects.find(subject => subject.value === this.props.subject)
+                    .label
+                }
+              </span>
+            </div>
+            <div>
+              所在地:&nbsp;
+              <span>
+                {
+                  regions.find(region => region.value === this.props.region)
+                    .label
+                }
+              </span>
+            </div>
           </div>
           <div className="product-rating ">
-            {!this.props.selected ?
-              <button type="button" className="button button-3d button-rounded" onClick={() => { this.addToList(); }} style={{ backgroundColor: '#0ebb99' }}>
+            {!this.props.selected ? (
+              <button
+                type="button"
+                className="button button-3d button-rounded"
+                onClick={() => {
+                  this.addToList();
+                }}
+                style={{ backgroundColor: "#0ebb99" }}
+              >
                 <i className="icon-bookmark2" />
                 加入詢問清單
               </button>
-              :
-              <button type="button" className="button button-3d button-rounded" onClick={() => { this.removeFromList(); }} style={{ backgroundColor: '#7CBAB7' }}>
+            ) : (
+              <button
+                type="button"
+                className="button button-3d button-rounded"
+                onClick={() => {
+                  this.removeFromList();
+                }}
+                style={{ backgroundColor: "#7CBAB7" }}
+              >
                 <i className="icon-bookmark2" />
                 移除詢問清單
-              </button>}
+              </button>
+            )}
           </div>
         </div>
       </div>
@@ -68,17 +139,17 @@ Talent.propTypes = {
   selected: PropTypes.bool.isRequired,
   photo: PropTypes.string.isRequired,
   removeCandidate: PropTypes.func.isRequired,
-  candidates: PropTypes.arrayOf(PropTypes.number),
+  candidates: PropTypes.arrayOf(PropTypes.number)
 };
 
 Talent.defaultProps = {
-  candidates: [],
+  candidates: []
 };
 
 function mapStateToProps(state) {
-  return { 
+  return {
     candidates: state.candidates,
-    authenticated:state.authenticated
+    authenticated: state.authenticated
   };
 }
 

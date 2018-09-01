aacd7434be225166b65b5dc88f86c3260a562267
JulianChu <yulang.chu@gmail.com>
2018-09-01 23:23:34 +0200

    tuning css

--- src/components/talents/Filter.jsx
+++ src/components/talents/Filter.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import {setFilters} from '../../actions/index';
-import regions from '../common/regions';
-import subjects from '../common/subjects';
-import languages from '../common/languages';
-
-
+import React, { Component } from "react";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { setFilters } from "../../actions/index";
+import regions from "../common/regions";
+import subjects from "../common/subjects";
+import languages from "../common/languages";
 
 class Filter extends Component {
   constructor(props) {
@@ -22,15 +20,15 @@ class Filter extends Component {
       selectedRegions: [],
       selectAllRegionsIsChecked: false,
       selectedSubjects: [],
-      selectAllSubjectsIsChecked: false,
-    }
+      selectAllSubjectsIsChecked: false
+    };
   }
 
   langsChange(val) {
     this.setState({
       selectedLanguages: val,
       selectAllLangsIsChecked: false
-    })
+    });
   }
 
   selectAllLangs() {
@@ -38,20 +36,20 @@ class Filter extends Component {
       this.setState({
         selectedLanguages: [],
         selectAllLangsIsChecked: false
-      })
+      });
     } else {
       this.setState({
         selectedLanguages: this.langOptions,
         selectAllLangsIsChecked: true
-      })
+      });
     }
   }
 
   regionsChange(val) {
     this.setState({
       selectedRegions: val,
-      selectAllRegionsIsChecked:false
-    })
+      selectAllRegionsIsChecked: false
+    });
   }
 
   selectAllRegions() {
@@ -59,12 +57,12 @@ class Filter extends Component {
       this.setState({
         selectedRegions: [],
         selectAllRegionsIsChecked: false
-      })
+      });
     } else {
       this.setState({
         selectedRegions: this.regionOptions,
         selectAllRegionsIsChecked: true
-      })
+      });
     }
   }
 
@@ -72,7 +70,7 @@ class Filter extends Component {
     this.setState({
       selectedSubjects: val,
       selectAllSubjectsIsChecked: false
-    })
+    });
   }
 
   selectAllSubjects() {
@@ -80,77 +78,119 @@ class Filter extends Component {
       this.setState({
         selectedSubjects: [],
         selectAllSubjectsIsChecked: false
-      })
+      });
     } else {
       this.setState({
         selectedSubjects: this.subjectOptions,
         selectAllSubjectsIsChecked: true
-      })
+      });
     }
   }
 
   submitFilters(event) {
-    event.preventDefault()
-    let filters = [this.state.selectedRegions, this.state.selectedLanguages, this.state.selectedSubjects];
+    event.preventDefault();
+    let filters = [
+      this.state.selectedRegions,
+      this.state.selectedLanguages,
+      this.state.selectedSubjects
+    ];
     this.props.setFilters(filters);
   }
 
   render() {
     return (
-      <form onSubmit={(e) => this.submitFilters(e)}>
+      <form onSubmit={e => this.submitFilters(e)}>
         <div>
-          <div style={{ display: 'inline-block', maxWidth: '400px', minWidth: '300px'}}>
+          <div
+            style={{
+              display: "inline-block",
+              maxWidth: "400px",
+              minWidth: "300px"
+            }}
+          >
             <Select
               multi
               options={this.regionOptions}
-              onChange={(val) => this.regionsChange(val)}
+              onChange={val => this.regionsChange(val)}
               placeholder="Select regions"
               closeOnSelect={false}
               value={this.state.selectedRegions}
               // style={{ width: '400px', maxHeight: '20px', overflow: 'hidden' }}
-              style={{ maxWidth: '300px' }}
+              style={{ maxWidth: "300px" }}
             />
             <label htmlFor="" className="checkbox">
-              <input type="checkbox" name="selectAllRegions" checked={this.state.selectAllRegionsIsChecked} onChange={() => this.selectAllRegions()} />
+              <input
+                type="checkbox"
+                name="selectAllRegions"
+                checked={this.state.selectAllRegionsIsChecked}
+                onChange={() => this.selectAllRegions()}
+              />
               <span className="checkbox-label">全選</span>
             </label>
           </div>
 
-          <div style={{ display: 'inline-block', maxWidth: '400px', minWidth: '200px' }}>
+          <div
+            style={{
+              display: "inline-block",
+              maxWidth: "400px",
+              minWidth: "200px"
+            }}
+          >
             <Select
               multi
               options={this.langOptions}
-              style={{ maxWidth: '250px' }}
-              onChange={(val) => this.langsChange(val)}
+              style={{ maxWidth: "250px" }}
+              onChange={val => this.langsChange(val)}
               placeholder="Select languages"
               closeOnSelect={false}
               value={this.state.selectedLanguages}
             />
             <label htmlFor="" className="checkbox">
-              <input type="checkbox" name="selectAllLangs" checked={this.state.selectAllLangsIsChecked} onChange={() => this.selectAllLangs()} />
+              <input
+                type="checkbox"
+                name="selectAllLangs"
+                checked={this.state.selectAllLangsIsChecked}
+                onChange={() => this.selectAllLangs()}
+              />
               <span className="checkbox-label">全選</span>
             </label>
           </div>
 
-          <div style={{ display: 'inline-block', maxWidth: '400px', minWidth: '200px' }}>
+          <div
+            style={{
+              display: "inline-block",
+              maxWidth: "400px",
+              minWidth: "200px"
+            }}
+          >
             <Select
               multi
               options={this.subjectOptions}
-              style={{ maxWidth: '250px' }}
-              onChange={(val) => this.subjectsChange(val)}
+              style={{ maxWidth: "250px" }}
+              onChange={val => this.subjectsChange(val)}
               placeholder="Select subjects"
               closeOnSelect={false}
               value={this.state.selectedSubjects}
             />
             <label htmlFor="" className="checkbox">
-              <input type="checkbox" name="selectAllsubjects" checked={this.state.selectAllSubjectsIsChecked} onChange={() => this.selectAllSubjects()} />
+              <input
+                type="checkbox"
+                name="selectAllsubjects"
+                checked={this.state.selectAllSubjectsIsChecked}
+                onChange={() => this.selectAllSubjects()}
+              />
               <span className="checkbox-label">全選</span>
             </label>
           </div>
-          <button type="submit" className="btn btn-default" >Filter</button>
+          <button
+            type="submit"
+            className="btn btn-default btn-success filter-button"
+          >
+            Filter
+          </button>
         </div>
       </form>
-    )
+    );
   }
 }
 
@@ -164,4 +204,7 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({ setFilters }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(Filter);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Filter);
